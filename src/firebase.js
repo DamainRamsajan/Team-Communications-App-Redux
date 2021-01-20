ed119c1e938c1a6ab82bddf4a8e190d56ff436c3
Damain Ramsajan <dpramsajan@gmail.com>
2021-01-20 22:09:51 +0100

    Redux template setup and cleanedup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyApWZcVoyTJrRcHSPMM1NUAIj4cwoo7x8s",
+    authDomain: "teams-comms-app-redux.firebaseapp.com",
+    projectId: "teams-comms-app-redux",
+    storageBucket: "teams-comms-app-redux.appspot.com",
+    messagingSenderId: "438554843191",
+    appId: "1:438554843191:web:9ce6c238bea8c5706707b2"
+  };
